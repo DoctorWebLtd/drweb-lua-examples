8c7d91b2bc8cb9c7239e7e900a0c4693890af0a7
Alexandr Morokov <a.morokov@drweb.com>
2018-10-22 15:59:31 +0300

    modifier become a table

--- maild/milter/modifications_subject.lua
+++ maild/milter/modifications_subject.lua
@@ -8,7 +8,7 @@ function milter_hook(ctx)
     local subject = ""
     -- Устанавливаем переменной modifier, функцию для осуществления модификаций
     -- Set the variable modifier, the function for implementing the modifications
-    local modifier = ctx.modifier()
+    local modifier = ctx.modifier
 
     -- Получаем тему письма из заголовков
     -- Get the subject of the message from the headers
