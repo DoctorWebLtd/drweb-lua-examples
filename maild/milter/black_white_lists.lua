9e03215ce5875453c81bdf532f9970a1224c06c1
Alexandr Morokov <a.morokov@drweb.com>
2019-03-11 17:43:25 +0300

    regex.match and regex.search: can take array of patterns

--- maild/milter/black_white_lists.lua
+++ maild/milter/black_white_lists.lua
@@ -7,26 +7,38 @@ local regex = require "drweb.regex"
     -- regex.match(pattern, text [, flags])
 
 -- Load regexp patterns from files
-local whitelist = drweb.load_set("/opt/drweb.com/lists/whitemails.txt")
-local blacklist = drweb.load_set("/opt/drweb.com/lists/blackmails.txt")
+local whitelist = drweb.load_array("/opt/drweb.com/lists/whitemails.txt")
+local blacklist = drweb.load_array("/opt/drweb.com/lists/blackmails.txt")
 
 
 -- Entry point to check email message sent to the Dr.Web MailD by Rspamd protocol
 function milter_hook(ctx)
 
     -- Stop checks if mail_from matchs one of the patterns loaded from file
-    for pattern, _ in pairs(whitelist) do
+    for _, pattern in ipairs(whitelist) do
         if regex.match(pattern, ctx.from, regex.ignore_case) then
             return {action = "accept"}
         end
     end
 
     -- Stop checks if mail_from matchs one of the patterns loaded from file
-    for pattern, _ in pairs(blacklist) do
+    for _, pattern in ipairs(blacklist) do
         if regex.match(pattern, ctx.from, regex.ignore_case) then
             return {action = "reject", message = "Blacklist"}
         end
     end
+
+    -- regex.match and regex.search can also taking arrays of patterns
+    -- and the code above will be pretty simple:
+
+    -- -- Stop checks if mail_from matchs one of the patterns loaded from file
+    -- if regex.match(whitelist, ctx.from, regex.ignore_case) then
+    --     return {action = "accept" }
+    -- end
+    -- -- Stop checks if mail_from matchs one of the patterns loaded from file
+    -- if regex.match(blacklist, ctx.from, regex.ignore_case) then
+    --     return {action = "reject", message = "Blacklist" }
+    -- end
 
     return {action = "accept"}
 end
